c7dd58c8944a5eaed28fd9c5e13cda0f0edb3c59
Kirill <guzz1988@mail.ru>
2022-08-30 21:29:13 +0300

    i can find all max

--- findmaxfrom3/Program.cs
+++ findmaxfrom3/Program.cs
@@ -5,3 +5,17 @@ if (max < b) max = b;
 
 
 Console.WriteLine(max);
+
+int c = 2;
+int d = 10;
+int max1 = c;
+if (max1 < d) max1 = d;
+
+Console.WriteLine(max1);
+
+int e = -9;
+int f = -3;
+int max2 = e;
+if (max2 < f) max2 = f;
+
+Console.WriteLine(max2);
