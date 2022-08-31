838a4e8c73378fea7cec25a44bcf7a6a91030a50
Kirill <guzz1988@mail.ru>
2022-08-31 15:23:50 +0300

    i can find max from 3 final

--- findmaxmaxfrom3/Program.cs
+++ findmaxmaxfrom3/Program.cs
@@ -1,27 +1,41 @@
-﻿int a = 2;
-int b = 3;
-int c = 7;
+﻿Console.WriteLine ("Введите числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
 int max = a;
 if (max < b) max = b;
 if (max < c) max = c;
+Console.WriteLine(max);
 
-Console.WriteLine (max);
 
 
-int d = 44;
-int e = 5;
-int f = 78;
-int max1 = d;
-if (max1 < e) max1 = e;
-if (max1 < f) max1 = f;
 
-Console.WriteLine (max1);
 
-int g = 22;
-int h = 3;
-int i = 9;
-int max2 = g;
-if (max2 < h) max2 = h;
-if (max2 < i) max2 = i;
 
-Console.WriteLine (max2);
+//int a = 2;
+//int b = 3;
+//int c = 7;
+//int max = a;
+//if (max < b) max = b;
+//if (max < c) max = c;
+
+//Console.WriteLine (max);
+
+
+//int d = 44;
+//int e = 5;
+//int f = 78;
+//int max1 = d;
+//if (max1 < e) max1 = e;
+//if (max1 < f) max1 = f;
+
+//Console.WriteLine (max1);
+
+//int g = 22;
+//int h = 3;
+//int i = 9;
+//int max2 = g;
+//if (max2 < h) max2 = h;
+//if (max2 < i) max2 = i;
+
+//Console.WriteLine (max2);
