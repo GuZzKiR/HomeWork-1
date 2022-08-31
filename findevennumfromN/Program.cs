e625243e70b2282c354942bbda566a81a3929a8f
Kirill <guzz1988@mail.ru>
2022-08-31 16:03:23 +0300

    i can find evennum from N

--- findevennumfromN/Program.cs
+++ findevennumfromN/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine ("Введите число:");
+int N =  Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while ( i < N ) {
+    if(i % 2 == 0){
+    Console.Write( i + " " );
+    }
+    i++;
+}  
