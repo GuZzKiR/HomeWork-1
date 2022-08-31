7abc6d8d8b5f58e637ba9eb97d45804f9c39b499
Kirill <guzz1988@mail.ru>
2022-08-31 14:56:18 +0300

    i can find evennum

--- findevennum/Program.cs
+++ findevennum/Program.cs
@@ -1,3 +1,9 @@
-﻿int a = 4,
-    b = -3,
-    c = 7;
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int c = 0;
+if( c ==n % 2  )  
+    Console.WriteLine ("Число четное!");
+if ( c < n % 2)
+    Console.WriteLine ("Число нечетное!"); 
+if ( c > n % 2)
+    Console.WriteLine ("Число нечетное!");       
