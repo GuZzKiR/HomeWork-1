1efe5f007f53a39185708ebad47ffc277caf31d5
Kirill <guzz1988@mail.ru>
2022-09-02 18:36:48 +0300

    massivecool

--- massive/Program.cs
+++ massive/Program.cs
@@ -1,25 +1,23 @@
-﻿void FillArray(int[] collection)
+﻿void FillArray (int[]collection)
 {
     int length = collection.Length;
     int index = 0;
-    while(index < length)
+    while (index < length)
     {
-        collection[index] = new Random().Next (1,10);
-        index++;
+    collection[index] = new Random().Next(1,10);
+    index++;
     }
 }
-
-void PrintArray(int[] col)
+void PrintArray(int[]col)
 {
     int count = col.Length;
     int pos = 0;
-   while(pos < count)
-   {
-       Console.WriteLine(col[pos]);
-       pos ++;
-   }
+    while(pos < count)
+    {
+        Console.WriteLine (col[pos]);
+        pos ++;
+    }
 }
-
-int[] array = new int [10];
+int[] array = new int[10];
 FillArray(array);
 PrintArray(array);
