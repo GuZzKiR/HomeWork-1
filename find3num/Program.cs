0cd414a1dce51bcaab735ab39463334cd0368cb1
Kirill <guzz1988@mail.ru>
2022-09-03 16:24:11 +0300

    find2cool

---
+++ find3num/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число: "); 
+while(true)
+{   
+    string input = Console.ReadLine( );
+        Console.WriteLine("{0}-{1}", input, input[1]);
+        break;
+}
+
+
+
+
