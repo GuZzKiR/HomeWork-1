c0827d3983aa7cbc18f13a1f4c3d53ece6ca5fbf
Kirill <guzz1988@mail.ru>
2022-09-03 15:57:03 +0300

    generatenum

---
+++ generateRandom/Program.cs
@@ -0,0 +1,15 @@
+﻿int GenerateRandomNumber()
+{
+    int n = new Random().Next(100, 1000);
+    return n;
+}
+void CompraiseNumber ()
+{ 
+    int n= GenerateRandomNumber();
+    Console.WriteLine(n);
+    int a = n / 100;
+    int b = n % 10;
+    Console.WriteLine(a * 10 + b);
+}
+
+CompraiseNumber();
