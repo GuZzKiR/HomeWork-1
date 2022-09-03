b39e0a1e4f7df1b479e53fbf0ac67c0d26b122bf
Kirill <guzz1988@mail.ru>
2022-09-03 17:55:30 +0300

    i can find 3 but

---
+++ findnumfrom3to3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+Console.WriteLine("Введите число: ");
+int num()
+{
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+void numbers()
+{
+    int n = num();
+        Console.WriteLine(Convert.ToString(n)[2]);
+}
+numbers();
