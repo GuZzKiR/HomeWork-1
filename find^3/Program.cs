27dfe7e34a7a9942837acca9f4531cd794af664a
Kirill <guzz1988@mail.ru>
2022-09-07 18:23:59 +0300

    icanfind^3andnameuser

--- find^3/Program.cs
+++ find^3/Program.cs
@@ -1,7 +1,21 @@
 ﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 Console.Clear();
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+
+    Console.WriteLine();
+
+Console.Write("Привет,  ");
+Console.WriteLine(username+"!");
+
+    Console.WriteLine();
+
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= n; i++)
     Console.WriteLine(i + "^3="+ i * i*i);
+    Console.WriteLine();
+
+Console.Write("Ты красавчик,  ");
+Console.WriteLine(username+"!");
