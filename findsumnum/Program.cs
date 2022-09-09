6396679677452fbe01c8bb4f829424895506a1aa
Kirill <guzz1988@mail.ru>
2022-09-09 20:46:03 +0300

    i can findstepen

--- findsumnum/Program.cs
+++ findsumnum/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵);2, 4 -> 16
+
+
+Console.WriteLine("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
