54b30e033c6e6f33af1f8a15bbd460fbb243ebad
Kirill <guzz1988@mail.ru>
2022-09-09 20:44:21 +0300

    i can findst

---
+++ findsumnum/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int num = 1;
+
+for(int i = 0; i<b; i++)
+{
+    num *=a;
+}
+Console.WriteLine(num);
+Console.Write(a + "^" + b);
+
+//Console.WriteLine(Math.Pow(a,b));
+//Console.Write(a+"^"+b);
