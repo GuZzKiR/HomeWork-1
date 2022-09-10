54af34eb71955736cc9b121a980cb576ae9320db
Kirill <guzz1988@mail.ru>
2022-09-10 13:28:28 +0300

    pickupberry

---
+++ picking berries/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите колличество кустов черники: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] berry = new int[n];
+int sum=0;
+for(int i=0; i<berry.Length; i++)
+berry[i]=new Random().Next(1, n+1);
+Console.WriteLine("[" +string.Join(" ,", berry) + "]");
+for(int i=0; i<berry.Length; i++)
+{
+    sum = i+(i+1);
+}
+Console.WriteLine(sum);
