b090d14dd11a8df5d0268d1d64a3422570fae7e5
Kirill <guzz1988@mail.ru>
2022-09-14 21:11:24 +0300

    uneven sum finded

---
+++ sumuneveni/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+Console.Clear();
+Console.WriteLine("Поиск суммы элементов, стоящих на нечётных позициях.");
+
+void InputArray(int[] array) 
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 10);
+}
+
+int SumArray(int[] array)
+{
+    int sum = 0;
+    
+    for(int i=1; i<array.Length; i=i+2)
+    {
+        if(i % 2!=0)
+        sum=sum+array[i];
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+    Console.WriteLine("Массив состоит из " + n + " элементов!");
+        Console.WriteLine();
+int[] array = new int[n];
+    InputArray(array);
+    Console.WriteLine("Исходный массив данных: [" + string.Join(", ", array) + "]");
+        Console.WriteLine();
+            Console.WriteLine("Сумма элементов, стоящих на нечётных позициях: " + SumArray(array));
