3dd35d7dd2e7c6091cb5d075ef0e9aca24dfcf82
Kirill <guzz1988@mail.ru>
2022-09-14 14:35:24 +0300

    changemassive

---
+++ changenegandpos/Program.cs
@@ -0,0 +1,20 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-9, 10);
+}
+
+int[] Change (int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = (-1)*(array[i]);
+    }
+    return array;
+}
+
+int[] arr = new int[12];
+InputArray(arr);
+
+Console.WriteLine("Исходный массив данных: [" + string.Join(", ", arr) + "]");
+Console.WriteLine("Конечный массив данных: [" + string.Join(", ", Change(arr)) + "]");
