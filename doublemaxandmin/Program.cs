a693bc15dc4a9a76fe069c518427d9a8f8de6a80
Kirill <guzz1988@mail.ru>
2022-09-15 13:21:27 +0300

    i can find diff min and max

---
+++ doublemaxandmin/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задайте массив дробных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Clear();
+Console.WriteLine("Программа ищет разницу между максимальным и минимальным элементов массива.");
+Console.WriteLine("-----------");
+
+Console.WriteLine("Введите необхожимое число элементов в массиве: ");
+int n =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-----------");
+double[] array = new double[n]; 
+   for (int i = 0; i < array.Length; i++)
+{
+   array[i] = new Random().NextDouble() * 100;
+}
+Console.WriteLine("Исходный массив данных: [" + string.Join(", ", array) + "]");
+double max =array[0];
+double min =array[0];
+
+for(int i=0; i<array.Length; i++)
+{
+    if(max < array[i])
+    {
+    max = array[i];
+    }
+        if(min > array[i])
+        {
+        min = array[i];
+        }
+}
+    double result=max - min;
+    Console.WriteLine("-----------");
+        Console.WriteLine("Разница между максимальным: [" + (max) +"] и минимальным: ["+ (min) +"] элементами: ["+ (result)+"]");
+   
+
+
+
+
