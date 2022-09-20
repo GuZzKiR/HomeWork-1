17426e3e1aa3167c02bf04d173a4d7af72230603
Kirill <guzz1988@mail.ru>
2022-09-20 15:38:30 +0300

    transponation matrix done

---
+++ transponation/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задана целочисленная матрица, состоящая из N строк и M столбцов. Требуется транспонировать ее относительно горизонтали.
+
+// Входные данные
+// Первая строка входного файла INPUT.TXT содержит два натуральных числа N и M – количество строк и столбцов матрицы. 
+// В каждой из последующих N строк записаны M целых чисел – элементы матрицы. Все числа во входных данных не превышают 100 по абсолютной величине.
+
+// Выходные данные
+// В выходной файл OUTPUT.TXT выведите матрицу, полученную транспонированием исходной матрицы относительно горизонтали.
+
+Console.WriteLine("Программа  которая транспоинирует матрицу по горизонтали.");
+Console.WriteLine("Добро пожаловать!");
+
+Console.WriteLine("-----------");
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 10); //заполнение мтарицы
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+        for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + " \t"); //вывод матрицы
+        Console.WriteLine();
+    }
+}
+void ReleaseMatrix(int[,] matrix)
+{
+    int[,] ResultMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            ResultMatrix[i, j] = matrix[matrix.GetLength(0) - i - 1, j];
+        Console.WriteLine();
+    }
+    Console.WriteLine("Смещенная матрица: ");
+    PrintMatrix(ResultMatrix);
+}
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
+
