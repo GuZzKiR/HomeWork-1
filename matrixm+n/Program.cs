97fcaf1e2bd2318bbb13603bdda28825d6e5bd9a
Kirill <guzz1988@mail.ru>
2022-09-20 15:52:49 +0300

    feel matrix i+j

---
+++ matrixm+n/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = i + j; //заполнение мтарицы
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + " \t"); //вывод матрицы
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+PrintMatrix(matrix);
