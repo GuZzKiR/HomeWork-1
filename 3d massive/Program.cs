097a2867a6466459e99f131da892d970186f0b5c
Kirill <guzz1988@mail.ru>
2022-09-24 17:27:30 +0300

    3d mass done

---
+++ 3d massive/Program.cs
@@ -0,0 +1,44 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+
+Console.Clear();
+Console.WriteLine("Программа  которая будет построчно выводить массив, добавляя индексы каждого элемента.");
+Console.WriteLine("Добро пожаловать!");
+int[,,] matrix3D = new int[2, 2, 2];
+FillMatrix(matrix3D);
+Console.WriteLine("_______");
+Console.WriteLine("Массив и его индексы: ");
+PrintIndex(matrix3D);
+void PrintIndex(int[,,] matrix3D)
+{
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                Console.Write($"{matrix3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+// Функция заполнения 3D массива не повторяющимеся числами
+void FillMatrix(int[,,] matrix3D)
+{
+    int count = 10;
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                matrix3D[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
