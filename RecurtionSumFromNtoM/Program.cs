53555c6315764346b58c45d0e202753a03666ea3
Kirill <guzz1988@mail.ru>
2022-09-29 16:53:14 +0300

    sum from M to N done

---
+++ RecurtionSumFromNtoM/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+Console.WriteLine("Программа которая которая найдёт сумму натуральных элементов в промежутке от M до N.");
+Console.WriteLine("Добро пожаловать!");
+
+Console.WriteLine("-----------");
+
+Console.WriteLine("Введите изначальное число: ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите финальное число: ");
+int N = int.Parse(Console.ReadLine());
+
+void SumNumberFromMtoN(int M, int N, int sum)
+{
+    if (M > N)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (M++);
+    SumNumberFromMtoN(M, N, sum);
+}
+Console.WriteLine("-----------");
+SumNumberFromMtoN(M, N, 0);
+
