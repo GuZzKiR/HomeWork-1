a1e92ae8c3a1d9b90b22ee7d550681908cd42844
Kirill <guzz1988@mail.ru>
2022-10-10 22:47:43 +0300

    get print LastWork

--- LastWork/Program.cs
+++ LastWork/Program.cs
@@ -1,32 +1,57 @@
 ﻿// Написать программу которая из имеющегося массива строк формирует массив из строк,
 // длина которых меньше либо равна 3.
- Console.Clear();
+Console.Clear();
 Console.WriteLine("Программа которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3.");
 Console.WriteLine("Добро пожаловать!");
 Console.WriteLine();
 
 
-		string[] array1 = {"Hello", "2", "world", ":-)"};
-		string[] array2 = {"1234", "1567", "-2", "computer science"};
-		string[] array3 = {"Russia", "Denmark", "Kazan"};
-		
-		int word_length = 3;
+string[] array1 = { "Hello", "2", "world", ":-)" };
+string[] array2 = { "1234", "1567", "-2", "computer science" };
+string[] array3 = { "Russia", "Denmark", "Kazan" };
 
-      string[] GetShortWords(string[] array)
-		{
-			int length = array.Length;			
-			string[] result = new string[length];
-			int count = 0;
-			
-			for (int i = 0; i < length; i++)
-			{
-				if(array[i].Length <= word_length)
-				{
-					result[count] = array[i];
-					count++;
-				}
-			}
-			Array.Resize(ref result, count);
-			
-			return result;
-		}  
+int word_length = 3;
+
+string[] GetShortWords(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= word_length)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+
+    return result;
+}
+void PrintArray(string[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine("В массиве нет подхочящего значения");
+    }
+    else
+    {
+        Array.ForEach(array, (str) => Console.Write($"{str} "));
+        Console.WriteLine();
+    }
+}
+void PrintTask(string[] array)
+{
+    Console.WriteLine("Исходящий массив:");
+    PrintArray(array);
+    string[] shortArray = GetShortWordsArray(array);
+    Console.WriteLine("Получившийся массив:");
+    PrintArray(shortArray);
+    Console.WriteLine();
+}
+
+PrintTask(array1);
+PrintTask(array2);
+PrintTask(array3);
