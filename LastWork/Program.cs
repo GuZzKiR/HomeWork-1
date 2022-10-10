a87654c3e988980737eb09f86eb356445569711d
Kirill <guzz1988@mail.ru>
2022-10-10 22:46:14 +0300

    get short words

--- LastWork/Program.cs
+++ LastWork/Program.cs
@@ -11,8 +11,8 @@ Console.WriteLine();
 		string[] array3 = {"Russia", "Denmark", "Kazan"};
 		
 		int word_length = 3;
-		
-		string[] GetShortWordsArray(string[] array)
+
+      string[] GetShortWords(string[] array)
 		{
 			int length = array.Length;			
 			string[] result = new string[length];
@@ -29,32 +29,4 @@ Console.WriteLine();
 			Array.Resize(ref result, count);
 			
 			return result;
-		}
-		
-		void PrintArray(string[] array)
-		{
-			if(array.Length == 0)
-			{
-				Console.WriteLine("В массиве нет подхочящего значения");
-			}
-			else
-			{
-				Array.ForEach(array, (str) => Console.Write($"{str} "));
-				Console.WriteLine();
-			}
-		}
-		
-		void PrintTask(string[] array)
-		{
-			Console.WriteLine("Исходящий массив:");
-			PrintArray(array);
-			string[] shortArray = GetShortWordsArray(array);
-			Console.WriteLine("Получившийся массив:");
-			PrintArray(shortArray);
-			Console.WriteLine();
-		}
-		
-		PrintTask(array1);
-		PrintTask(array2);
-		PrintTask(array3);
-
+		}  
