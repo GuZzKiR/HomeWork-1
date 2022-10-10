a2472b580d9e93d91f214e8584a7ab861eeb66aa
Kirill <guzz1988@mail.ru>
2022-10-10 23:07:24 +0300

    get massive

--- Verification work/Program.cs
+++ Verification work/Program.cs
@@ -4,3 +4,13 @@ Console.Clear();
 Console.WriteLine("Программа которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3.");
 Console.WriteLine("Добро пожаловать!");
 Console.WriteLine();
+
+
+string[] array1 = new string[4] { "Hello", "2", "world", ":-)" } ;
+string[] arraycopy1 = new string[array1.Length];
+
+string[] array2 = new string[5] { "1234", "1567", "-2", "computer science" };
+string[] arraycopy2 = new string[array2.Length];
+
+string[] array3 = new string[3] { "Russia", "Denmark", "Kazan" };
+string[] arracopy3 = new string[array3];
